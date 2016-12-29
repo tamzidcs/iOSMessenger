f729ff6a3201accf7dfd4c489ca1227dca3557c4
Tamzid Hasan Chowdhury <tamzid@tamzids-macbook-pro.local>
2016-12-28 23:26:14 -0500

    Initial Commit

---
+++ iOS Messenger/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  iOS Messenger
+//
+//  Created by Tamzid Hasan Chowdhury on 12/28/16.
+//  Copyright © 2016 Tamzid Hasan Chowdhury. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iOS Messenger/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iOS Messenger
+//
+//  Created by Tamzid Hasan Chowdhury on 12/28/16.
+//  Copyright © 2016 Tamzid Hasan Chowdhury. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
