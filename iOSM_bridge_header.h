27287a77f37a0543e699cae0c802acae850f5962
Tamzid Hasan Chowdhury <tamzid@tamzids-macbook-pro.local>
2016-12-30 04:15:21 -0500

    Added XMPP Framework Version 3.6.5

---
+++ iOSM_bridge_header.h
@@ -0,0 +1,13 @@
+//
+//  iOSM_bridging_header.h
+//  iOS Messenger
+//
+//  Created by Tamzid Hasan Chowdhury on 12/28/16.
+//  Copyright © 2016 Tamzid Hasan Chowdhury. All rights reserved.
+//
+
+#ifndef iOSM_bridging_header_h
+#define iOSM_bridging_header_h
+
+
+#endif /* iOSM_bridging_header_h */
---
+++ test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  iOS Messenger
+//
+//  Created by Tamzid Hasan Chowdhury on 12/29/16.
+//  Copyright © 2016 Tamzid Hasan Chowdhury. All rights reserved.
+//
+
+#ifndef test_h
+#define test_h
+
+
+#endif /* test_h */
